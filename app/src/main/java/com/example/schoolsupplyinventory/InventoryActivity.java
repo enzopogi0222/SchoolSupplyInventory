648709006293992001cb060ad449eb23e5ce195b
enzopogi0222 <enzopro2222@gmail.com>
2026-04-27 14:31:25 +0800

    implementing Activity Lifecycle and State Persistence to restore the data if the app was closed

--- app/src/main/java/com/example/schoolsupplyinventory/InventoryActivity.java
+++ app/src/main/java/com/example/schoolsupplyinventory/InventoryActivity.java
@@ -2,6 +2,8 @@ package com.example.schoolsupplyinventory;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -10,12 +12,19 @@ public class InventoryActivity extends AppCompatActivity {
 
     private Button mAvailableButton;
     private Button mBorrowedButton;
+    private static final String TAG = "InventoryActivity";
+    private static final String KEY_INDEX = "index";
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // This line connects the Java code to the XML layout
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         // Wiring the 'Available' Button
         mAvailableButton = (Button) findViewById(R.id.available_button);
@@ -37,5 +46,46 @@ public class InventoryActivity extends AppCompatActivity {
                         R.string.mark_as_borrowed, Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+        //Save which item index the user was looking at
+        outState.putInt(KEY_INDEX, mCurrentIndex);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+        // Logic: Save temporary data here if needed
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
